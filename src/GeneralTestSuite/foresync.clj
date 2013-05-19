bfcc861163e4b1a515cf1f5e0b043b01040d8554
Chen Li <garfield@crunchnet.(none)>
2013-05-19 22:37:10 +0800

    asyn load file info

--- src/GeneralTestSuite/foresync.clj
+++ src/GeneralTestSuite/foresync.clj
@@ -20,6 +20,7 @@
 (defn list-dir 
   "List all files under a directory, 
   returns a list of java.io.File.
+  Unlike file-seq, this function oes not walk into sub-directories.
    Deprecated, use clojure.core.file-seq"
   [file-loc]
   (let [f (as-file file-loc)] 
@@ -54,6 +55,39 @@
           md5sum (if (.isDirectory f) nil
                    (digest/md5 f))]
         (recur (rest new-file-list) (cons {:name name :size size :md5sum md5sum} result-list))))))
+
+(defn get-file-info*
+  "Get file information of a file. Potentially time consuming."
+  [f]
+  ;TODO Should filter ignored file/dir 
+  (let [name (.getName f)
+        size (if (.isDirectory f) nil 
+                 (.length f))
+        md5sum (if (.isDirectory f) nil
+                   (digest/md5 f)) ]
+    {:name name :size size :md5sum md5sum}))
+
+(defn get-file-info
+  [f]
+  (future (get-file-info* f)))
+
+(defn list-files-concurr*
+  [file-list]
+  (loop [new-file-list file-list
+         result-list '()]
+    (if (empty? new-file-list) 
+        result-list
+        (recur (rest new-file-list)
+               (cons (get-file-info (first new-file-list))
+                     result-list)))))
+
+(defn list-files-concurr
+  "List file information concurrently. 
+  Usage: (def future-files (doall (list-files-concurr file-list)))
+  Note use doall to realize the map operation immediately."
+  [file-list]
+  (pmap get-file-info file-list))
+
 
 (defn list-files 
   "Get a sequence of file info in the specified directory"
